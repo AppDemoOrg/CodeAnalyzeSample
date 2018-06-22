85566f2a4e68219653cd37c20c2348c602cd5344
huangweiqi <askviky2010@gmail.com>
2018-06-23 00:04:18 +0800

    完善代码检查

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# CodeAnalyzeSample
+## CodeAnalyzeSample    
+
+### Android静态代码分析    
+1、Java代码规范检测工具CheckStyle     
+2、Java静态代码分析工具FindBugs      
+3、Java静态代码分析工具PMD       
+4、Android代码优化工具Lint      
+
+### 代码分析配置目录    
+./config    
+
+### 参考资料    
+1、Android高级进阶第49章     
--- app/src/main/java/com/abt/codeanalyze/MainActivity.java
+++ app/src/main/java/com/abt/codeanalyze/MainActivity.java
@@ -1,13 +1,29 @@
 package com.abt.codeanalyze;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final Handler mHandler = new Handler(new Handler.Callback() {
+
+        @Override
+        public boolean handleMessage(Message msg) {
+            return false;
+        }
+    });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        }, 1000);
     }
 }
